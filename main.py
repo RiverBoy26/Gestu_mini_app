46d0ba18af146f761787a585a4ea4121b43cfbb6
4315 Ледовских А.В. <ledartem@bk.ru>
2025-09-19 21:54:18 +0300

    Первоначальное создание Telegram-бота

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+
+
+async def main():
+    load_dotenv()
+    bot = Bot(token=os.getenv('TOKEN'))
+    dp = Dispatcher()
+    ## dp.include_router(router)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Exit")
