4f949b2c701915d5047c0b41417dca90d2b16623
MarikSmerch <arambo202@gmail.com>
2025-10-09 00:04:22 +0300

    added handlers. Reorganizing the backend architecture

--- app/backend/main.py
+++ app/backend/main.py
@@ -3,13 +3,16 @@ from dotenv import load_dotenv
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from bot.handlers import start
 
 
 async def main():
     load_dotenv()
     bot = Bot(token=os.getenv('TOKEN'))
     dp = Dispatcher()
-    ## dp.include_router(router)
+    # dp.include_router(router)
+    dp.message.register(start, CommandStart())
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
