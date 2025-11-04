c3a94cc8071230dcf5a8277b227d00c86405a359
MarikSmerch <arambo202@gmail.com>
2025-11-05 00:12:02 +0300

    added base database and initialization

---
+++ app/backend/db/requests.py
@@ -0,0 +1,32 @@
+from . import get_session
+from .models import User, Lesson
+
+
+def add_user(username: str):
+    session = get_session()
+    new_user = User(username=username)
+    session.add(new_user)
+    session.commit()
+    session.close()
+
+
+def get_user_by_username(username: str):
+    session = get_session()
+    user = session.query(User).filter_by(username=username).first()
+    session.close()
+    return user
+
+
+def add_lesson(title: str, description: str, content_url: str, lesson_order: int):
+    session = get_session()
+    new_lesson = Lesson(title=title, description=description, content_url=content_url, lesson_order=lesson_order)
+    session.add(new_lesson)
+    session.commit()
+    session.close()
+
+
+def get_all_lessons():
+    session = get_session()
+    lessons = session.query(Lesson).all()
+    session.close()
+    return lessons
