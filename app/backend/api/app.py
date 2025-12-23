dbc9860264f5e257627eef1d4fc537b0cbefca75
MarikSmerch <arambo202@gmail.com>
2025-12-23 14:40:22 +0300

    update app file

--- app/backend/api/app.py
+++ app/backend/api/app.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
-from .routes import lessons, sessions, detections
+from fastapi.middleware.cors import CORSMiddleware
+
+from .routes import lessons, categories, sessions, detections
 from app.backend.api.ws import router as ws_router
 
 app = FastAPI(title="Gestu API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(categories.router)
 app.include_router(lessons.router)
 app.include_router(sessions.router)
 app.include_router(detections.router)
