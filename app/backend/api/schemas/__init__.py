4ab844a9b90da427971802f57233c03ead8cbb61
MarikSmerch <arambo202@gmail.com>
2025-12-23 14:46:59 +0300

    update app file

--- app/backend/api/schemas/__init__.py
+++ app/backend/api/schemas/__init__.py
@@ -2,3 +2,4 @@ from .lesson import LessonOut
 from .gesture_card import GestureCardOut
 from .session import SessionStartIn, SessionStartOut, SessionFinishIn
 from .detection import DetectionIn, DetectionsBulkIn
+from .category import CategoryOut
